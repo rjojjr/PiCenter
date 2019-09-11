aaeeed20203d492e3ba0608437f7f391c4c1fc43
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2019-09-11 14:41:20 -0500

    Initial commit

---
+++ src/main/java/com/kirchnersolutions/PiCenter/PiCenterApplication.java
@@ -0,0 +1,13 @@
+package com.kirchnersolutions.PiCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiCenterApplication.class, args);
+	}
+
+}
