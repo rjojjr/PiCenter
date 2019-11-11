9feab411c12ea4fd382907efd65ee1f060c8554c
Robert Kirchner JR <rjojjr91@live.com>
2019-11-11 17:45:47 -0600

    add sass styling

--- src/main/ui/src/comps/summary/SummaryPageHeader.js
+++ src/main/ui/src/comps/summary/SummaryPageHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SummaryPageHeader = ({selectSensor, summary}) => {
     return(
-    <div className={"summaryHeader"}>
+    <div className={"summaryHeader headerButtonContainer"}>
         <h3>Sensors</h3>
         {summary.map((sum, index) => (
             <button key={index} className={"summarySensorSelector"} onClick={() => {selectSensor(index)}}>{sum.roomName}</button>
