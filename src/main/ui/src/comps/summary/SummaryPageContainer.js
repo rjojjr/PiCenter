321ac1729173898be23b4e3bf0e5aac378c68a59
Robert Kirchner JR <rjojjr91@live.com>
2019-11-10 18:36:53 -0600

    wip: summary page container

--- src/main/ui/src/comps/summary/SummaryPageContainer.js
+++ src/main/ui/src/comps/summary/SummaryPageContainer.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { loadSummaryThunk } from "../../actions/summary-actions";
+import SummaryPage from "./SummaryPage";
 
-const SummaryPageContainer = ({}) => {
+const SummaryPageContainer = ({
+  loadSummary,
+  summary,
+  user,
+  isLoading,
+  isError,
+  errorMsg
+}) => {
 
-    return (
-        <div className={"pageContainer summaryPage"}>
-            
+    let sensorIndex = 0;
+
+    const selectSensor = (index) => {
+        sensorIndex = index;
+    }
+
+  return (
+    <div className={"container summaryPageContainer"}>
+      {summary === [] &&
+        !isLoading &&
+        !isError &&
+        user !== {} &&
+        loadSummary(user)}
+      {isLoading && <p>Loading summary...</p>}
+      {summary !== [] && !isLoading && !isError && user !== {} && (
+        <div className={"summaryHeader"}>
+            {summary.map((sum, index) => {(
+            <button key={index} className={"summarySensorSelector"} >{summary.roomName}</button>
+        )})}
+
         </div>
-    )
+      )}
+    </div>
+  );
 };
 
 const mapStateToProps = state => ({
-    
+  summary: state.summary,
+  user: state.user,
+  isLoading: state.isSummaryLoading,
+  isError: state.isSummaryError,
+  errorMsg: state.message
 });
 
 const mapDispatchToProps = {
-    
+  loadSummary: loadSummaryThunk
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(SummaryPageContainer);
