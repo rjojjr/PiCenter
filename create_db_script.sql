25a850e5f721615eb5e8becd6d8863c7954e03ad
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 13:04:06 -0600

    update background image

--- create_db_script.sql
+++ create_db_script.sql
@@ -1,3 +1,4 @@
-create database SHome;
+create database shome;
 create user 'SHomeAdmin'@'%' identified by 'kORCn7upp12tCHKNjA6AzVBrONaHqJqKXSVTymVpx44=';
-grant select, insert, delete, update on Shome.* to 'SHomeAdmin'@'%';
+/*grant select, insert, delete, update on shome.* to 'SHomeAdmin'@'%';*/
+grant all on shome.* to 'SHomeAdmin'@'%';
