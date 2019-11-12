b40d9f19fb10b8ad08f39c3ed0a959d43840e2f6
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 10:17:00 -0600

    add sql scripts

---
+++ create_db_script.sql
@@ -0,0 +1,3 @@
+create database SHome;
+create user 'SHomeAdmin'@'%' identified by 'kORCn7upp12tCHKNjA6AzVBrONaHqJqKXSVTymVpx44=';
+grant select, insert, delete, update on Shome.* to 'SHomeAdmin'@'%';
