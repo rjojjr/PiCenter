d271201ad06652c20e19abb45c210253f7a47920
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 23:30:38 -0600

    add left content header

---
+++ src/main/ui/src/comps/global/LeftContentHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LeftContentHeader = ({children}) => {
+    return(
+
+    <div className={"top-left"}>
+        {children}
+    </div>
+)};
+export default LeftContentHeader;
