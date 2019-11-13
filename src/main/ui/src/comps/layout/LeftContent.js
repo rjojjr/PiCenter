f9f8311c2c362c6240f315dac9d3a28a5eb3c3c4
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 23:33:20 -0600

    add left content

---
+++ src/main/ui/src/comps/layout/LeftContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LeftContent = ({children}) => {
+    return(
+
+        <div className={"bottom"}>
+            {children}
+        </div>
+    )};
+export default LeftContent;
