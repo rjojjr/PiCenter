362c603cfb2fc566213715a0d7784548107f027f
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 23:38:08 -0600

    add main content

---
+++ src/main/ui/src/comps/layout/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MainContent = ({children}) => {
+    return(
+
+        <div className={"bottom"}>
+            {children}
+        </div>
+    )};
+export default MainContent
