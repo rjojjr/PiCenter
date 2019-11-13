800c0c112cfe8a3b333f4b97698c1cc0246b998d
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 23:35:25 -0600

    add left content container

---
+++ src/main/ui/src/comps/layout/LeftContentContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LeftContentContainer = ({children}) => {
+    return(
+
+        <div id={"left"} className={"column"}>
+            {children}
+        </div>
+    )};
+export default LeftContentContainer;
