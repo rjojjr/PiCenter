71ac03f1ef59a3534dfe4a96675787be181fe94b
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 23:40:35 -0600

    add main content container

---
+++ src/main/ui/src/comps/layout/MainContentConainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MainContentContainer = ({children}) => {
+    return(
+
+        <div id={"right"} className={"column"}>
+            {children}
+        </div>
+    )};
+export default MainContentContainer;
