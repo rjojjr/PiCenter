2fcdb28dd6662f4bba451953134412ea4389451e
Robert Kirchner JR <rjojjr91@live.com>
2019-11-16 14:01:14 -0600

    add users link to summary page nav bar

--- src/main/ui/src/comps/summary/SummaryPageContainer.js
+++ src/main/ui/src/comps/summary/SummaryPageContainer.js
@@ -1,78 +1,82 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import SummaryPage from "./SummaryPage";
 import SummaryPageHeader from "./SummaryPageHeader";
 import LoadingView from "../global/LoadingView";
 
 import * as pageConstants from '../../constants/page-constants';
 
+import {changePage} from "../../actions/universal-actions";
+
 const SummaryPageContainer = ({
-  summary,
-  user,
-  isError,
-  errorMsg,
-  isLoading,
-  canLoad,
-  logOff
-}) => {
-  const [sensorIndex, setSensorIndex] = useState(0);
+                                  summary,
+                                  user,
+                                  isError,
+                                  errorMsg,
+                                  isLoading,
+                                  canLoad,
+                                  logOff
+                              }) => {
+    const [sensorIndex, setSensorIndex] = useState(0);
 
-  const selectSensor = index => {
-    setSensorIndex(index);
-  };
+    const selectSensor = index => {
+        setSensorIndex(index);
+    };
 
-  const showSummary = () => {
-    if (summary.length === 0) {
-      return "";
-    } else {
-      return summary[sensorIndex];
-    }
-  };
+    const showSummary = () => {
+        if (summary.length === 0) {
+            return "";
+        } else {
+            return summary[sensorIndex];
+        }
+    };
 
-  return (
-    <div className={"container summaryPageContainer"}>
-      <LoadingView isLoading={isLoading} message={"Loading.."} />
-      {canLoad && !isLoading && (
-        <div>
-          <header>
-            <h2>PiCenter Sensor Summary</h2>
-            
-          </header>
-          <div id="main">
-            <section>
-              <header >
-                <SummaryPageHeader
-                    isLoading={isLoading}
-                    summary={summary}
-                    selectSensor={selectSensor}
-                />
-                
-              </header>
-              <SummaryPage
-                canRender={canLoad}
-                isLoading={isLoading}
-                summary={showSummary()}
-              />
-            </section>
-            <nav>
-              <b>Summary</b>
-              <ul>
-                <a onClick={}
-              </ul>
-            </nav>
-            <aside>
-              <h4>Logged on as: {user.userName}</h4>
-              <button type={"button"} onClick={logOff}>
-                Logout
-              </button>
-            </aside>
-          </div>
-          <footer>
-            <a href={"github.com/rjojjr"}>Visit me on github</a>
-          </footer>
+    return (
+        <div className={"container summaryPageContainer"}>
+            <LoadingView isLoading={isLoading} message={"Loading.."}/>
+            {canLoad && !isLoading && (
+                <div>
+                    <header>
+                        <h2>PiCenter Sensor Summary</h2>
+
+                    </header>
+                    <div id="main">
+                        <section>
+                            <header>
+                                <SummaryPageHeader
+                                    isLoading={isLoading}
+                                    summary={summary}
+                                    selectSensor={selectSensor}
+                                />
+
+                            </header>
+                            <SummaryPage
+                                canRender={canLoad}
+                                isLoading={isLoading}
+                                summary={showSummary()}
+                            />
+                        </section>
+                        <nav>
+                            <ul>
+                                <b>Summary</b>
+                                <ul>
+                                    <a onClick={() => changePage(pageConstants.USERS)}>Users</a>
+                                </ul>
+                            </ul>
+                        </nav>
+                        <aside>
+                            <h4>Logged on as: {user.userName}</h4>
+                            <button type={"button"} onClick={logOff}>
+                                Logout
+                            </button>
+                        </aside>
+                    </div>
+                    <footer>
+                        <a href={"github.com/rjojjr"}>Visit me on github</a>
+                    </footer>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default SummaryPageContainer;
--- src/main/ui/src/constants/page-constants.js
+++ src/main/ui/src/constants/page-constants.js
@@ -1,7 +1,11 @@
 export const LOADING_PAGE = '/loading';
 export const MSG_PAGE = '/msg';
 export const SUMMARY_PAGE = '/summary';
+
 export const LOGIN_PAGE = '/login';
 export const LOGOUT = '/logout';
 export const UPDATE_SESSION = '/update';
+
+export const USERS = '/users';
 export const CREATE_USER = '/users/create';
+
