b902ad94aa5f69b09f0c831f66d117a4100d07d2
Robert Kirchner JR <rjojjr91@live.com>
2019-11-16 12:44:44 -0600

    Added session update rest bean

--- src/main/java/com/kirchnersolutions/PiCenter/servers/beans/SessionUpdate.java
+++ src/main/java/com/kirchnersolutions/PiCenter/servers/beans/SessionUpdate.java
@@ -1,7 +1,12 @@
 package com.kirchnersolutions.PiCenter.servers.beans;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SessionUpdate {
+
+    private String page = "null";
+
 }
