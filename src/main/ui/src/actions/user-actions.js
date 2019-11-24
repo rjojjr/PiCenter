efde1b629c90301ee0a6b723420cf16204763faf
Robert Kirchner <rjojjr91@live.com>
2019-11-24 09:37:21 -0600

    add create user thunk

--- src/main/ui/src/actions/user-actions.js
+++ src/main/ui/src/actions/user-actions.js
@@ -1,7 +1,22 @@
+import {load, createUser from "../services/axios-service";
+import {setUser} from "./universal-actions";
+import * as debugConstants from "../constants/debug-constants";
+import {isLoading, isNotLoading, loadingError} from "./loader-actions";
+
 export const USERS_IS_LOADING = 'USERS_IS_LOADING'
 export const usersIsLoading = (loading) => ({
     type: USERS_IS_LOADING,
     loading
+});
+
+export const RESET_SHOW_MSG = 'RESET_SHOW_MSG'
+export const resetSHowMsg = () => ({
+    type: RESET_SHOW_MSG
+});
+
+export const USERS_SHOW_MSG = 'USERS_SHOW_MSG'
+export const sHowMsg = (msg) => ({
+    type: USERS_SHOW_MSG
 });
 
 export const USERS_DONE_LOADING = 'USERS_DONE_LOADING'
@@ -14,3 +29,19 @@ export const usersLoadingError = (msg, error) => ({
     type: USERS_LOADING_ERROR,
     msg, error
 });
+
+export const createUserThunk = () => async dispatch => {
+    try {
+        dispatch(usersIsLoading(true));
+        const initiationResponse = await createUser();
+
+        dispatch(setUser(user));
+        dispatch(isNotLoading());
+        return
+    } catch (error) {
+        if (process.env.NODE_ENV === 'development' && debugConstants.ALERT_DEBUG_THUNKS){
+            alert(error);
+        }
+        dispatch(loadingError('Network error'));
+    }
+};
