75c938238770a449cf56327aa82bf696717305c1
Robert Kirchner JR <rjojjr91@live.com>
2019-12-01 16:41:22 -0600

    fix ignored file

---
+++ src/main/java/com/kirchnersolutions/PiCenter/entites/DBItem.java
@@ -0,0 +1,13 @@
+package com.kirchnersolutions.PiCenter.entites;
+
+public interface DBItem {
+
+    public String getCSVHeader();
+
+    public String toCSV();
+
+    public void fromCSV(String csv, boolean withId);
+
+    public String getType();
+
+}
