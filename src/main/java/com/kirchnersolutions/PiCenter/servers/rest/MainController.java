20a669c8ce489139899963bc764397f1ac3e9671
Robert Kirchner JR <rjojjr91@live.com>
2019-12-01 18:05:16 -0600

    add /data/backup endpoint

--- src/main/java/com/kirchnersolutions/PiCenter/servers/rest/MainController.java
+++ src/main/java/com/kirchnersolutions/PiCenter/servers/rest/MainController.java
@@ -27,7 +27,7 @@ public class MainController {
     private CSVService csvService;
 
     @Autowired
-    public MainController(UserService userService, SummaryService summaryService, DebuggingService debuggingService, CSVService csvService){
+    public MainController(UserService userService, SummaryService summaryService, DebuggingService debuggingService, CSVService csvService) {
         this.userService = userService;
         this.summaryService = summaryService;
         this.debuggingService = debuggingService;
@@ -35,145 +35,172 @@ public class MainController {
     }
 
     @GetMapping("/loading")
-    public RestResponse initClient(HttpServletResponse response) throws Exception{
+    public RestResponse initClient(HttpServletResponse response) throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         //ServletWebRequest servletWebRequest=new ServletWebRequest(request);
         //HttpServletResponse response=servletWebRequest.getResponse();
         HttpSession httpSession = cookie(request, response);
-        if(httpSession.getAttribute("username") == null){
+        if (httpSession.getAttribute("username") == null) {
             return new RestResponse();
         }
-        return new RestResponse(userService.getRestUser((String)httpSession.getAttribute("username")));
+        return new RestResponse(userService.getRestUser((String) httpSession.getAttribute("username")));
     }
 
     @PostMapping("/login")
-    public RestResponse login(HttpServletResponse response, @RequestBody LogonForm logonForm) throws Exception{
+    public RestResponse login(HttpServletResponse response, @RequestBody LogonForm logonForm) throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         HttpSession httpSession = cookie(request, response);
-        if(httpSession.getAttribute("username") == null){
+        if (httpSession.getAttribute("username") == null) {
             AppUser user = userService.logOn(logonForm.getUsername(), logonForm.getPassword(), request.getRemoteAddr());
-            if(user != null){
+            if (user != null) {
                 httpSession.setAttribute("username", user.getUserName());
                 return new RestResponse(userService.getRestUser(user.getUserName()));
             }
             return new RestResponse("{ body: 'Wrong username or password' }");
         }
-        return new RestResponse(userService.getRestUser((String)httpSession.getAttribute("username")));
+        return new RestResponse(userService.getRestUser((String) httpSession.getAttribute("username")));
     }
 
     @GetMapping("/logout")
-    public RestResponse logoutClient(HttpServletResponse response) throws Exception{
+    public RestResponse logoutClient(HttpServletResponse response) throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         //ServletWebRequest servletWebRequest=new ServletWebRequest(request);
         //HttpServletResponse response=servletWebRequest.getResponse();
         HttpSession httpSession = cookie(request, response);
-        if(httpSession.getAttribute("username") == null){
+        if (httpSession.getAttribute("username") == null) {
             return new RestResponse();
         }
-        if(userService.logOff((String)httpSession.getAttribute("username"))){
+        if (userService.logOff((String) httpSession.getAttribute("username"))) {
             debuggingService.trace("user logged off");
             httpSession.setAttribute("username", null);
-            if(httpSession.getAttribute("username") == null){
+            if (httpSession.getAttribute("username") == null) {
                 debuggingService.trace("HTTP user is null ");
             }
             return new RestResponse("{body: logged off}");
         }
-        debuggingService.trace("Failed to log off user " + (String)httpSession.getAttribute("username"));
+        debuggingService.trace("Failed to log off user " + (String) httpSession.getAttribute("username"));
         return new RestResponse("{body: log off failed}");
     }
 
     @GetMapping("/summary")
-    public RestResponse showSummary(HttpServletResponse response, @RequestParam String userId ) throws Exception{
+    public RestResponse showSummary(HttpServletResponse response, @RequestParam String userId) throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         HttpSession httpSession = cookie(request, response);
-        if(httpSession.getAttribute("username") == null){
+        if (httpSession.getAttribute("username") == null) {
             return new RestResponse();
         }
-        if(userId == null || userId.toCharArray().length < 5){
-            userService.systemInvalidateUser((String)httpSession.getAttribute("username"), "unauthentic session");
+        if (userId == null || userId.toCharArray().length < 5) {
+            userService.systemInvalidateUser((String) httpSession.getAttribute("username"), "unauthentic session");
             return new RestResponse("{body: 'error', error: 'invalid token'}");
         }
-        if(!updateSession((String)httpSession.getAttribute("username"), userId, request.getRemoteAddr(), "/summary")){
+        if (!updateSession((String) httpSession.getAttribute("username"), userId, request.getRemoteAddr(), "/summary")) {
             return new RestResponse("{body: 'error', error: 'unauthentic session'}", new RestUser());
         }
-        return new RestResponse("{body: 'success'}", userService.getRestUser((String)httpSession.getAttribute("username")), summaryService.getRoomSummaries(2));
+        return new RestResponse("{body: 'success'}", userService.getRestUser((String) httpSession.getAttribute("username")), summaryService.getRoomSummaries(2));
     }
 
     @GetMapping("data/csv")
-    public RestResponse getCSV(HttpServletResponse response, @RequestParam String userId, @RequestParam String table) throws Exception{
+    public RestResponse getCSV(HttpServletResponse response, @RequestParam String userId, @RequestParam String table) throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         HttpSession httpSession = cookie(request, response);
-        if(httpSession.getAttribute("username") == null){
+        if (httpSession.getAttribute("username") == null) {
             return new RestResponse();
         }
-        if(userId == null || userId.toCharArray().length < 5){
-            userService.systemInvalidateUser((String)httpSession.getAttribute("username"), "unauthentic session");
+        if (userId == null || userId.toCharArray().length < 5) {
+            userService.systemInvalidateUser((String) httpSession.getAttribute("username"), "unauthentic session");
             return new RestResponse("{body: 'error', error: 'invalid token'}");
         }
-        if(!updateSession((String)httpSession.getAttribute("username"), userId, request.getRemoteAddr(), "/data/csv")){
+        if (!updateSession((String) httpSession.getAttribute("username"), userId, request.getRemoteAddr(), "/data/csv")) {
             return new RestResponse("{body: 'error', error: 'unauthentic session'}", new RestUser());
         }
-        if(!userService.isAdmin((String)httpSession.getAttribute("username"))){
-            return new RestResponse("{body: 'failed not authorized'}", userService.getRestUser((String)httpSession.getAttribute("username")));
+        if (!userService.isAdmin((String) httpSession.getAttribute("username"))) {
+            return new RestResponse("{body: 'failed not authorized'}", userService.getRestUser((String) httpSession.getAttribute("username")));
         }
-        if(table != null){
-            if(csvService.generateDownload(table)){
+        if (table != null) {
+            if (csvService.generateDownload(table)) {
                 /*String url = "http://picenter.kirchnerbusinesssolutions.com/download/backup";
                 response.setHeader("Location", url);
                 response.setStatus(302);*/
-                return new RestResponse("{body: 'success'}", userService.getRestUser((String)httpSession.getAttribute("username")));
+                return new RestResponse("{body: 'success'}", userService.getRestUser((String) httpSession.getAttribute("username")));
             }
         }
-        return new RestResponse("{body: 'failed to generate package'}", userService.getRestUser((String)httpSession.getAttribute("username")));
+        return new RestResponse("{body: 'failed to generate package'}", userService.getRestUser((String) httpSession.getAttribute("username")));
+    }
+
+    @GetMapping("data/backup")
+    public RestResponse getCSV(HttpServletResponse response, @RequestParam String userId) throws Exception {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest();
+        HttpSession httpSession = cookie(request, response);
+        if (httpSession.getAttribute("username") == null) {
+            return new RestResponse();
+        }
+        if (userId == null || userId.toCharArray().length < 5) {
+            userService.systemInvalidateUser((String) httpSession.getAttribute("username"), "unauthentic session");
+            return new RestResponse("{body: 'error', error: 'invalid token'}");
+        }
+        if (!updateSession((String) httpSession.getAttribute("username"), userId, request.getRemoteAddr(), "/data/backup")) {
+            return new RestResponse("{body: 'error', error: 'unauthentic session'}", new RestUser());
+        }
+        if (!userService.isAdmin((String) httpSession.getAttribute("username"))) {
+            return new RestResponse("{body: 'failed not authorized'}", userService.getRestUser((String) httpSession.getAttribute("username")));
+        }
+        if (csvService.generateManualBackup()) {
+                /*String url = "http://picenter.kirchnerbusinesssolutions.com/download/backup";
+                response.setHeader("Location", url);
+                response.setStatus(302);*/
+            return new RestResponse("{body: 'success'}", userService.getRestUser((String) httpSession.getAttribute("username")));
+        }
+        return new RestResponse("{body: 'failed to generate package'}", userService.getRestUser((String) httpSession.getAttribute("username")));
     }
 
     @PostMapping("/update")
-    public RestResponse updateSession(HttpServletResponse response, @RequestParam String userId, @RequestBody SessionUpdate sessionUpdate) throws Exception{
+    public RestResponse updateSession(HttpServletResponse response, @RequestParam String userId, @RequestBody SessionUpdate sessionUpdate) throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         HttpSession httpSession = cookie(request, response);
-        if(httpSession.getAttribute("username") == null){
+        if (httpSession.getAttribute("username") == null) {
             return new RestResponse();
         }
-        if(userId == null || userId.toCharArray().length < 5){
-            userService.systemInvalidateUser((String)httpSession.getAttribute("username"), "unauthentic session");
+        if (userId == null || userId.toCharArray().length < 5) {
+            userService.systemInvalidateUser((String) httpSession.getAttribute("username"), "unauthentic session");
             return new RestResponse("{body: 'error', error: 'invalid token'}");
         }
-        if(!updateSession((String)httpSession.getAttribute("username"), userId, request.getRemoteAddr(), sessionUpdate.getPage())){
+        if (!updateSession((String) httpSession.getAttribute("username"), userId, request.getRemoteAddr(), sessionUpdate.getPage())) {
             return new RestResponse("{body: 'error', error: 'unauthentic session'}", new RestUser());
         }
-        return new RestResponse("{body: 'success updating session'}", userService.getRestUser((String)httpSession.getAttribute("username")));
+        return new RestResponse("{body: 'success updating session'}", userService.getRestUser((String) httpSession.getAttribute("username")));
     }
 
     @PostMapping("/users/create")
-    public RestResponse creatUser(HttpServletResponse response, @RequestParam String token, @RequestBody CreateUser createUser) throws Exception{
+    public RestResponse creatUser(HttpServletResponse response, @RequestParam String token, @RequestBody CreateUser createUser) throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         HttpSession httpSession = cookie(request, response);
-        if(httpSession.getAttribute("username") == null){
+        if (httpSession.getAttribute("username") == null) {
             return new RestResponse();
         }
-        if(token == null){
-            userService.systemInvalidateUser((String)httpSession.getAttribute("username"), "unauthentic session");
+        if (token == null) {
+            userService.systemInvalidateUser((String) httpSession.getAttribute("username"), "unauthentic session");
             return new RestResponse("{body: 'error', error: 'invalid token'}");
         }
-        if(!updateSession((String)httpSession.getAttribute("username"), token, request.getRemoteAddr(), "/users")){
+        if (!updateSession((String) httpSession.getAttribute("username"), token, request.getRemoteAddr(), "/users")) {
             return new RestResponse("{body: 'error', error: 'unauthentic session'}", new RestUser());
         }
-        if(userService.createUser((String)httpSession.getAttribute("username"), createUser)){
-            return new RestResponse("{body: 'success'}", userService.getRestUser((String)httpSession.getAttribute("username")));
+        if (userService.createUser((String) httpSession.getAttribute("username"), createUser)) {
+            return new RestResponse("{body: 'success'}", userService.getRestUser((String) httpSession.getAttribute("username")));
         }
-        return new RestResponse("{body: 'error', error; 'failed to create user'}", userService.getRestUser((String)httpSession.getAttribute("username")));
+        return new RestResponse("{body: 'error', error; 'failed to create user'}", userService.getRestUser((String) httpSession.getAttribute("username")));
     }
 
-    private boolean updateSession(String username, String token, String ip, String page) throws Exception{
+    private boolean updateSession(String username, String token, String ip, String page) throws Exception {
         debuggingService.trace("Update username: " + username + " token: " + token + " ip: " + ip + " page: " + page);
-        if(userService.updateSession(username, token, ip, page) == null){
+        if (userService.updateSession(username, token, ip, page) == null) {
             userService.systemInvalidateUser(username, "unauthentic session");
             return false;
         }
@@ -192,13 +219,13 @@ public class MainController {
             userCookie.setMaxAge(30 * 60 + 1);
             response.addCookie(userCookie);
             return session;
-        } else if(session != null){
+        } else if (session != null) {
             String sessionId = session.getId();
             Cookie userCookie = new Cookie("JSESSIONID", sessionId);
             userCookie.setMaxAge(30 * 60 + 1);
             response.addCookie(userCookie);
             return session;
-        }else{
+        } else {
             session = getSession();
             String sessionId = session.getId();
             Cookie userCookie = new Cookie("JSESSIONID", sessionId);
