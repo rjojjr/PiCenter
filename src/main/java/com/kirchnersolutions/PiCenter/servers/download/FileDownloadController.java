dcc45d6178adb1b95309621fdc8d0655a74bc06b
Robert Kirchner JR <rjojjr91@live.com>
2019-12-04 18:23:01 -0600

    add /backup/auto endpoint to download controller

--- src/main/java/com/kirchnersolutions/PiCenter/servers/download/FileDownloadController.java
+++ src/main/java/com/kirchnersolutions/PiCenter/servers/download/FileDownloadController.java
@@ -5,6 +5,7 @@ import com.kirchnersolutions.utilities.DeleteTools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,10 +25,32 @@ public class FileDownloadController {
     private DebuggingService debuggingService;
 
     @RequestMapping("/backup")
-    public void downloadBackup( HttpServletRequest request,
+    public void downloadCSV( HttpServletRequest request,
                               HttpServletResponse response) throws Exception{
         //Authorized user will download the file
         Path file = Paths.get("PiCenter/Backup/Download/PiCenterBackup.zip");
+        if (Files.exists(file)) {
+            response.setContentType("application/zip");
+            response.addHeader("Content-Disposition", "attachment; filename=PiCenterBackup.zip");
+            try {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        try {
+            file.toFile().delete();
+        }catch (Exception e){
+            debuggingService.nonFatalDebug("Failed to delete backup download file", e);
+        }
+    }
+
+    @RequestMapping("/backup/auto")
+    public void downloadBackup(HttpServletRequest request,
+                               HttpServletResponse response, @RequestParam String date) throws Exception{
+        //Authorized user will download the file
+        Path file = Paths.get("PiCenter/Backup/automated/PiCenterBackup_" + date + ".zip");
         if (Files.exists(file)) {
             response.setContentType("application/zip");
             response.addHeader("Content-Disposition", "attachment; filename=PiCenterBackup.zip");
