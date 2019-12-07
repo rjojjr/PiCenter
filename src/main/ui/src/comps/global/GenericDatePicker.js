ab7b8396a4b62b1777f9d5cac55f317e52c9911d
Robert Kirchner JR <rjojjr91@live.com>
2019-12-07 12:22:10 -0600

    finish GenericDatePicker

--- src/main/ui/src/comps/global/GenericDatePicker.js
+++ src/main/ui/src/comps/global/GenericDatePicker.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 
-const GenericDatePicker = ({}) => {
+/**
+ * Takes a currentDate and changeDate function from state.
+ * @param currentDate: Date
+ * @param changeDate: function(Date)
+ * @returns {*}
+ * @constructor
+ */
+const GenericDatePicker = ({currentDate, changeDate}) => {
+
+    //onst [date, setDate] = useState(new Date(Date.now()));
+
+    const handleChange = (date) =>{
+        changeDate(date);
+    };
+
+    return (
+        <DatePicker
+            selected={currentDate}
+            onChange={handleChange}
+        />
+    );
 
 };
 export default GenericDatePicker;
