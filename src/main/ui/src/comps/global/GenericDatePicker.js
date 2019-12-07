04cecf13b6e6500058e007bc99833eaea95feaf8
Robert Kirchner JR <rjojjr91@live.com>
2019-12-07 12:13:20 -0600

    add GenericDatePicker

---
+++ src/main/ui/src/comps/global/GenericDatePicker.js
@@ -0,0 +1,9 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+const GenericDatePicker = ({}) => {
+
+};
+export default GenericDatePicker;
