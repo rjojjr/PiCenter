4d934c76eb5ff714c4aac6d1b5d559cdc4c3ca59
Robert Kirchner JR <rjojjr91@live.com>
2019-12-15 15:45:34 -0600

    VisualPage.js switches chart types

--- src/main/ui/src/comps/data/Visual/VisualPage.js
+++ src/main/ui/src/comps/data/Visual/VisualPage.js
@@ -7,30 +7,30 @@ import Button from "react-bootstrap/Button";
 
 import Select from 'react-select';
 import {CHART_TYPES} from "../../../constants/page-constants";
+import DiffChart from "../../global/DiffChart";
 
 const VisualPage = ({tempChartStart, tempChartEnd, visualFromDate, visualToDate, chartData, getChart, setChartType, chartType, setChartFlavor, chartFlavor, intSelected}) => {
 
     const [typeSelected, setTypeSelected] = useState(intSelected);
     const [flavor, setFlavor] = useState(chartFlavor);
 
-
     const retrieveChart = () => {
         getChart(chartType);
     }
 
     const handleChange = (selected) => {
-        if (selected.value === typeSelected){
+        if (selected.value === typeSelected) {
             return;
         }
         setTypeSelected(selected.value);
-        if(selected.value > 2){
+        if (selected.value > 2) {
             setFlavor('hl');
-        }else{
+        } else {
             setFlavor('avg');
         }
-        if (selected.value === 1 || selected.value === 3){
+        if (selected.value === 1 || selected.value === 3) {
             setChartType('temp');
-        }else{
+        } else {
             setChartType('hum');
         }
         setChartFlavor(flavor);
@@ -40,26 +40,33 @@ const VisualPage = ({tempChartStart, tempChartEnd, visualFromDate, visualToDate,
         return CHART_TYPES[typeSelected - 1].label + ' Chart';
     }
 
-    return(
-      <div className={"page visualDataPage"}>
-          <div className={"dateSelectorContainer headerButtonContainer"}>
+    return (
+        <div className={"page visualDataPage"}>
+            <div className={"dateSelectorContainer headerButtonContainer"}>
 
-                  <p>
-                      <h4><b>{getChartLabel()}</b></h4>
-                      <Select options={CHART_TYPES} onChange={handleChange} value={typeSelected} placeholder={'Chart Type'}/>
-                      <GenericDatePicker currentDate={tempChartStart} changeDate={visualFromDate}>Start Date</GenericDatePicker>
-                      <GenericDatePicker currentDate={tempChartEnd} changeDate={visualToDate}>End Date</GenericDatePicker>
-                      <Button variant={"primary"} onClick={retrieveChart}>
-                          Get Chart
-                      </Button>
+                <p>
+                    <h4><b>{getChartLabel()}</b></h4>
+                    <Select options={CHART_TYPES} onChange={handleChange} value={typeSelected}
+                            placeholder={'Chart Type'}/>
+                    <GenericDatePicker currentDate={tempChartStart} changeDate={visualFromDate}>Start
+                        Date</GenericDatePicker>
+                    <GenericDatePicker currentDate={tempChartEnd} changeDate={visualToDate}>End Date</GenericDatePicker>
+                    <Button variant={"primary"} onClick={retrieveChart}>
+                        Get Chart
+                    </Button>
 
-                  </p>
-          </div>
+                </p>
+            </div>
 
+            {typeSelected < 3 && (
+                <TempChart data={chartData}/>
+            )}
 
+            {typeSelected > 2 && (
+                <DiffChart data={chartData}/>
+            )}
 
-              <TempChart data={chartData} />
-      </div>
+        </div>
     );
 
 };
