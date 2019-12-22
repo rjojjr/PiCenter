4b9c64c0ce6b13d63899c674fed3de4f6b0a4872
Robert Kirchner JR <rjojjr91@live.com>
2019-12-21 19:08:59 -0600

    add restartDHT to DeviceService

--- src/main/java/com/kirchnersolutions/PiCenter/services/DeviceService.java
+++ src/main/java/com/kirchnersolutions/PiCenter/services/DeviceService.java
@@ -91,4 +91,26 @@ public class DeviceService {
         return getDeviceStatus(name);
     }
 
+    private DeviceStatus restartDHT(String name){
+        Device device = deviceList.getDeviceByName(name);
+        if(device == null){
+            return null;
+        }
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.set("token", device.getToken()); // optional - in case you auth in headers
+        HttpEntity<String> entity = new HttpEntity<String>("", headers);
+        ResponseEntity<String> respEntity = restTemplate.exchange(device.getUrl() + "/kill/dht", HttpMethod.GET, entity, String.class);
+        if(!respEntity.getBody().equals("killed")){
+            return null;
+        }
+        entity = new HttpEntity<String>("", headers);
+        respEntity = restTemplate.exchange(device.getUrl() + "/start/dht", HttpMethod.GET, entity, String.class);
+        if(!respEntity.getBody().equals("started") || !respEntity.getBody().equals("running")){
+            return null;
+        }
+        return getDeviceStatus(name);
+    }
+
 }
