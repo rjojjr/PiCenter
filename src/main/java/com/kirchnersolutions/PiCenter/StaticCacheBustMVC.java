699cd098e15440de42c7c2e987af74ca537f5399
Robert Kirchner JR <rjojjr91@live.com>
2019-12-24 14:52:05 -0600

    cleanup

--- src/main/java/com/kirchnersolutions/PiCenter/StaticCacheBustMVC.java
+++ src/main/java/com/kirchnersolutions/PiCenter/StaticCacheBustMVC.java
@@ -1,4 +1,4 @@
-package com.kirchnersolutions.PiCenter.servers.rest;
+package com.kirchnersolutions.PiCenter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
