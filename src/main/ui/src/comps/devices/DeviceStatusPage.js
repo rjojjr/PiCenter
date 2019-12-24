1c9e88b7957fa5c54ac8aede1e25fcd4a0948471
Robert Kirchner JR <rjojjr91@live.com>
2019-12-24 11:48:33 -0600

    add DeviceIndicators ro DeviceStatusPage

--- src/main/ui/src/comps/devices/DeviceStatusPage.js
+++ src/main/ui/src/comps/devices/DeviceStatusPage.js
@@ -1,6 +1,23 @@
 import React from "react";
+import DeviceIndicator from "./DeviceIndicator";
+import {Button} from "react-bootstrap";
 
 const DeviceStatusPage = ({getStatuses, restartPiTemp, restartDHT, deviceData}) => {
-    
+
+    return (
+        <div className={"page deviceStatusPage"}>
+            <div className={"deviceIndicatorContainer"}>
+                {deviceData.map(device => {
+                    return (
+                        <DeviceIndicator device={device}
+                                         restartPiTemp={restartPiTemp}
+                                         restartDHT={restartDHT}/>
+                    );
+                })}
+            </div>
+            <Button variant={"primary"} onClick={getStatuses}>Refresh</Button>
+        </div>
+    );
+
 }
 export default DeviceStatusPage
