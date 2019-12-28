2115f0141c894cc9541fedb7e2e7bb69cac56113
Robert Kirchner JR <rjojjr91@live.com>
2019-12-28 14:24:38 -0600

    added device-service to ui

--- src/main/ui/src/services/device-service.js
+++ src/main/ui/src/services/device-service.js
@@ -1,16 +1,16 @@
 export const  addToLoading = (loadingList, loadingDevice) => {
-    loadingList.filter(device=== loadingDevice);
+    loadingList.filter(device => device === loadingDevice);
     loadingList.push(loadingDevice);
     return loadingList;
 }
 
 export const removeFromLoading = (loadingList, loadingDevice) => {
-    loadingList.filter(device === loadingDevice);
+    loadingList.filter(device => device === loadingDevice);
     return loadingList;
 }
 
-export const changeStatus = (deviceList, dvice) => {
-    deviceList.filter(deviceList.name === device.name);
+export const changeStatus = (deviceList, device) => {
+    deviceList.filter(list => list.name === device.name);
     deviceList.push(device);
     return deviceList;
 }
