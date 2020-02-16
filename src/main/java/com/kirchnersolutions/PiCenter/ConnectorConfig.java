f48b9881d66078dcb6c7beb126c3a52ae6d19c3a
Robert Kirchner JR <rjojjr91@live.com>
2020-02-16 11:23:40 -0600

    update lombok

--- src/main/java/com/kirchnersolutions/PiCenter/ConnectorConfig.java
+++ src/main/java/com/kirchnersolutions/PiCenter/ConnectorConfig.java
@@ -36,7 +36,7 @@ public class ConnectorConfig {
     private Connector redirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(7733);
         connector.setSecure(false);
         connector.setRedirectPort(443);
         return connector;
