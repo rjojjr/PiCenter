fa762edb0a2c5feb9306b7a5b499969b333a0a43
Robert Kirchner JR <admin@kirchnerbusinesssolutions.com>
2020-02-20 16:56:55 -0600

    add war builder add ping endpoint

--- src/main/java/com/kirchnersolutions/PiCenter/PiCenterApplication.java
+++ src/main/java/com/kirchnersolutions/PiCenter/PiCenterApplication.java
@@ -4,25 +4,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@RestController
 @EnableScheduling
-public class PiCenterApplication {
+public class PiCenterApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PiCenterApplication.class);
+	}
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(PiCenterApplication.class, args);
 	}
 
-	/*
-
-
-	 */
+	//TODO - More advanced ping mechanism.
+	@RequestMapping(value = "/ping")
+	public String ping() {
+		return "OKAY";
+	}
 }
